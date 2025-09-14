147edc78a27a0600ace1490139c081278da83936
kamruj-akash <source.akash@gmail.com>
2025-09-14 17:42:44 +0600

    Added Main-page news with API

--- js/index.js
+++ js/index.js
@@ -9,12 +9,42 @@ const loadNavMenu = async () => {
   const categoryMenu = data.categories;
 
   categoryMenu.forEach((category) => {
-    console.log(category);
     const newDiv = document.createElement("div");
-    newDiv.innerHTML = `<p class="border-b-4 border-transparent hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${category.title}</p>`;
+    newDiv.innerHTML = `<p onClick="showViaCategory('${category.id}')" id="${category.id}_btn" class="border-b-4 border-transparent hover:border-b-4 hover:border-red-600 border-red-600 cursor-pointer">${category.title}</p>`;
     navContainer.appendChild(newDiv);
+  });
+};
+
+const showViaCategory = (id) => {
+  const url = `https://news-api-fs.vercel.app/api/categories/${id}`;
+  console.log(url);
+};
+
+const loadAllCategoryNews = async () => {
+  const categoryContainer = getID("newsContainer");
+  const url = "https://news-api-fs.vercel.app/api/categories/main";
+  const res = await fetch(url);
+  const data = await res.json();
+  const articles = data.articles;
+
+  articles.forEach((article) => {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = ` 
+      <div class="border  border-gray-300 rounded-lg">
+            <div>
+             <img src="${article.image.srcset[5].url}"/>
+            </div>
+            <div id="${article.id}" class="p-2">
+                <h1 class="font-extrabold">${article.title}</h1>
+            <p class="text-sm">${article.time}</p>
+                 <button class="btn">Bookmark</button>
+                   <button class="btn">View Details</button>
+            </div>
+        </div>`;
+    categoryContainer.appendChild(newDiv);
   });
 };
 
 // default function call
 loadNavMenu();
+loadAllCategoryNews();
